6790e583aec68d8b76545ce8570bbd4a7f7282e6
dave <dave@cornforth.net>
2019-08-03 13:13:42 +0100

    search box placeholder

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 class Header extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            text : ''
+        }
+    }
 
     render(){
         return (
             <View >
-                <Text>HEADER</Text>
+                <View style={{flexDirection: 'row'}}>
+                    <TextInput 
+                        placeholder='Search...'
+                        onChangeText={(text) => this.setState({text})}
+                        value={this.state.text}
+                    />
+                    {/* <Text>test</Text> */}
+                </View>
             </View>
         )
     }
